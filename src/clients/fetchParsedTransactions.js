7d24ea7d4b29fdb415f0cc9dd8d135803b7ebfbe
127001sudais <127001sudais01@gmail.com>
2024-03-18 05:20:46 +0800

    this function is ok

--- src/clients/fetchParsedTransactions.js
+++ src/clients/fetchParsedTransactions.js
@@ -1,5 +1,4 @@
 import chalk from "chalk";
-
 import { Connection } from "@solana/web3.js";
 import { RPC_URL } from "../constants/constatnt.js";
 import { parseGetParsedTransactions } from "../utils/parseGetParsedTransactions.js";
@@ -8,34 +7,51 @@ let connection = new Connection(RPC_URL, "confirmed");
 
 export async function fetchParsedTransactions(transactionSignature) {
   try {
+    if (!transactionSignature || transactionSignature.length === 0) {
+      console.log(
+        chalk.bgYellow(
+          "Warning: transactionSignature array is empty or undefined."
+        )
+      );
+      return [];
+    }
+
     const config = {
       commitment: "confirmed",
       maxSupportedTransactionVersion: 0,
     };
+
+    console.log(
+      chalk.blue(
+        `[INFO] Fetching ${chalk.cyan.bold(
+          transactionSignature.length
+        )} transactions for signatures.`
+      )
+    );
 
     const parsedTransaction = await connection.getParsedTransactions(
       transactionSignature,
       config
     );
 
-    if (parsedTransaction) {
-      let filtered = parseGetParsedTransactions(parsedTransaction);
-      // console.log(`Sender: `, filtered.sender);
-      // console.log(`Reciever: `, filtered.receiver);
-      // console.log(`mintID: `, filtered[0].mintAddress);
-
-      return filtered;
-    } else {
+    if (!parsedTransaction || parsedTransaction.length === 0) {
       console.log(
-        chalk.blue("Info: No transaction found with the given signature.")
+        chalk.blue("Info: No transactions found with the given signatures.")
       );
       return [];
+    } else {
+      console.log(`Fetched ${parsedTransaction.length} transactions.`);
+      let filtered = parseGetParsedTransactions(parsedTransaction);
+      return filtered;
     }
   } catch (error) {
     console.error(
-      chalk.bgRed("Error fetching parsed transaction details:"),
+      chalk.red("[Error] fetching parsed transaction details:"),
       error.message
     );
     throw new Error("Failed to fetch parsed transaction details.");
   }
 }
+
+// 94ynwucj7htXFG2JWedUMjcPBuuaoerki3JoHCDwxENY
+// H24RXEMJ6TK61NrbMZoNxMj2u3yaxJcVMSM65AqfUj9o
